00b2649a1631e2de490508cd9ded1a8e07730691
huseyinekh <huseyinekh@code.edu.az>
2024-01-06 22:46:23 +0400

    create basic instruction

---
+++ src/components/UI/Form/FastTextField/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { TextField } from "@mui/material"
+const FastTextField = () => {
+  return (
+    <TextField label="asdad" />
+  )
+}
+
+export default FastTextField
